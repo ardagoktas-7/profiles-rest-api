ffd8de2a7f85e668047c2e90c1efd4bbb503bbf0
ardagoktas-7 <arda>
2021-05-07 17:54:56 +0300

    added login apı

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,9 +2,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
 
 from profiles_api import serializers
 from profiles_api import models
+from profiles_api import permissions
 
 # Create your views here.
 
@@ -89,3 +95,12 @@ class HelloViewSet(viewsets.ViewSet):
 class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user auth tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
