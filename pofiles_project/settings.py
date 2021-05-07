5d3916dd4be1dd7cd518e7b62942454adabd19aa
ardagoktas-7 <arda>
2021-05-07 19:44:07 +0300

    traceback

--- pofiles_project/settings.py
+++ pofiles_project/settings.py
@@ -79,6 +79,12 @@ WSGI_APPLICATION = 'pofiles_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
